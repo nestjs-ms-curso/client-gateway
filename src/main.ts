4da68cc9b0807733f3e2d1d125b6426432b56692
Max CM <maxcm260595@gmail.com>
2025-03-20 17:53:38 -0500

    feat: se agrego el health check

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common/exceptions';
 
 async function bootstrap() {
   const logger = new Logger('Main-Gateway');
 
   const app = await NestFactory.create(AppModule);
-  
-  app.setGlobalPrefix('api');
+
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
